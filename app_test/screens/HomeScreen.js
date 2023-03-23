72cece5d079997c0ac043b476b9d27f01f1c52d9
Seung Hun <youu0208@naver.com>
2023-03-23 14:52:53 +0900

    update

--- app_test/screens/HomeScreen.js
+++ app_test/screens/HomeScreen.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import {Button, View} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import * as LocalAuthentication from 'expo-local-authentication';
 
 function HomeScreen({navigation}) {
+    const [authenticated, setAuthenticated] = useState(false);
+
+    const handleAuth = async () => {
+        try {
+        const result = await LocalAuthentication.authenticateAsync();
+        if (result.success) {
+            setAuthenticated(true);
+        } else {
+            setAuthenticated(false);
+        }
+        } catch (error) {
+        console.log('Authentication error:', error);
+        }
+    };
     return (
-        <View>
+        <View style={styles.container}>
             <Button 
                 title="Detail 열기" 
                 onPress={()=> navigation.navigate('Detail')}
@@ -16,12 +31,29 @@ function HomeScreen({navigation}) {
                 title="QR코드 생성"
                 onPress={()=> navigation.navigate('QRcodemake')}
                 />
-            <Button 
-                title="생체 인증"
-                onPress={()=> navigation.navigate('BioScan')}
-                />
+            {authenticated ? (
+                alert('생체 인증 완료')
+            ) : (
+                <>
+                    <Button 
+                        title="생체 인증 버튼"
+                        onPress={()=> handleAuth()}>
+                    </Button>
+                </>
+            )}
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    text: {
+      fontSize: 16,
+    },
+});
 
 export default HomeScreen;
